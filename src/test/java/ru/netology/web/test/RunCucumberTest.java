1c3fc0a2bdd389be32ac4eacd79dc804f6c97904
Anastasia Fokina <45092650+stasechka11@users.noreply.github.com>
2024-02-29 13:58:23 +0400

    added RunCucumberTest

---
+++ src/test/java/ru/netology/web/test/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package ru.netology.web.test;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "summary"},
+        features = {"src/test/resources/features"},
+        glue = {"ru.netology.web.steps"})
+public class RunCucumberTest {
+}
